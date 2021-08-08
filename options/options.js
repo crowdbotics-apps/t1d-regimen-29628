531beffb60980649a8a3a2a48c62ac37143c2f18
Crowdbotics <team@crowdbotics.com>
2021-08-08 03:41:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "t1d_regimen_29628Identifier",
+  url: "https://t1d_regimen_29628Identifier.botics.co",
+  api: "https://t1d_regimen_29628Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
